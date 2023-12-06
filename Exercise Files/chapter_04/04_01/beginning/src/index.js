6ce5c182e8f940da55abd1b39bbd1e53ebafbfe9
l0ku <gonzalozanelli1@gmail.com>
2023-12-05 21:03:50 -0300

    finish 04_01

--- Exercise Files/chapter_04/04_01/beginning/src/index.js
+++ Exercise Files/chapter_04/04_01/beginning/src/index.js
@@ -4,10 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import Provider from './context.js';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
